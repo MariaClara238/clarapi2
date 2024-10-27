f147f5b31feb7a2b37d2397738870f7fd26349d9
MariaClara238 <clara.fonseca@escolar.ifrn.edu.br>
2024-10-27 18:35:59 -0300

    Enviando dados do formulário para aplicação

---
+++ src/main/java/ifrn/pi/eventos/models/Conexaobanco.java
@@ -0,0 +1,21 @@
+package ifrn.pi.eventos.models;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexaobanco {
+
+	    public static void main(String[] args) {
+	        // Configurações de conexão
+	        String url = "jdbc:mysql://localhost:3306/eventos"; 
+	        String usuario = "root"; 
+	        String senha = "20060329";     
+
+	        try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
+	            System.out.println("Conexão bem-sucedida!");
+	        } catch (SQLException e) {
+	            System.out.println("Erro ao conectar: " + e.getMessage());
+	        }
+	    }
+	}
+
